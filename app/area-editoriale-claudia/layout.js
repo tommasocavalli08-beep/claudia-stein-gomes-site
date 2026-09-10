02be20aa3f9460f84891ae6da7dd28bb22ab31b7
tommasocavalli08-beep <tommasocavalli.08@gmail.com>
2026-09-11 01:03:43 +0200

    feat: add editorial studio and embedded treatment videos

---
+++ app/area-editoriale-claudia/layout.js
@@ -0,0 +1,3 @@
+import './studio.css';
+export const metadata={title:'Área editorial',robots:{index:false,follow:false,nocache:true,googleBot:{index:false,follow:false,noimageindex:true}}};
+export default function Layout({children}){return children}
--- app/robots.js
+++ app/robots.js
@@ -1,1 +1,2 @@
-import {SITE_URL} from '@/lib/site';export default function robots(){return {rules:{userAgent:'*',allow:'/'},sitemap:`${SITE_URL}/sitemap.xml`,host:SITE_URL}}
+import {SITE_URL} from '@/lib/site';
+export default function robots(){return {rules:{userAgent:'*',allow:'/',disallow:['/area-editoriale-claudia','/area-editoriale-claudia/']},sitemap:`${SITE_URL}/sitemap.xml`,host:SITE_URL}}
